b8cd5adf7b724428fe1dc334675f99ccecce1ce3
jayrboy <j.jayox_@hotmail.com>
2024-03-16 22:00:07 +0700

    Update Code

--- modules/facebook/facebook.js
+++ modules/facebook/facebook.js
@@ -3,34 +3,26 @@ import data from './videoData.json' assert { type: 'json' }
 
 //! Access Token from Graph API: หมดอายุภายในเวลา 1-2 ชั่วโมง
 
-const params = {
-  access_token: data.accessToken,
+//TODO: User Meta from Graph API (Test)
+async function getUserAPI() {
+  const url = 'https://graph.facebook.com/v19.0/me'
+  const params = {
+    fields: 'id,name,email',
+    access_token: import.meta.env.VITE_ACCESS_TOKEN,
+  }
+  try {
+    const response = await axios.get(url, { params })
+    console.log(response.data)
+  } catch (err) {
+    alert(err)
+  }
 }
 
-export function getUser() {
-  // let url = `https://graph.facebook.com/v19.0/me?fields=id,name,email,picture&access_token=${data.accessToken}`
-  let url = `https://graph.facebook.com/v19.0/me?fields=id,name,email`
-  return axios(url, {
-    method: 'get',
-    params: params,
-  })
-    .then((result) => result.data)
-    .catch((e) => console.log({ message: e.message }))
-}
-
-export function getComment() {
-  let url = `https://graph.facebook.com/v19.0/me/comments?access_token=${data.accessToken}` // { data: [] }
-  return axios
-    .get(url)
-    .then((result) => result.data)
-    .catch((e) => console.log({ message: e.message }))
-}
-
-export function getLiveVideo() {
+async function getLiveVideoAPI() {
   let url = `http://graph.facebook.com/v19.0/me/live_videos?fields=id,title,status,permalink_url`
   // let url = `http://graph.facebook.com/v19.0/me/live_videos/?access_token=${data.accessToken}`
 
-  return axios
+  return await axios
     .get(url)
     .then((res) => {
       const liveVideo = res.data.data[0] // /24493349380312689/videos/758082652421417
@@ -49,30 +41,77 @@ export function getLiveVideo() {
     })
 }
 
-export function getLiveId() {
-  let url = `http://streaming-graph.facebook.com/v18.0/${liveId}/live_comments`
-  return axios
-    .get(url, {
-      params: params,
-    })
-    .then((result) => {
-      if (result && !result.error) {
-        console.log(result.data)
-      }
-    })
-    .catch((e) => res.status(500).send({ message: e.message }))
+async function getCommentAPI() {
+  const liveVideoID = data.liveId
+  const params = {
+    access_token: data.accessToken,
+  }
+  const url = `https://graph.facebook.com/v19.0/${liveVideoID}/comments`
+  try {
+    const response = await axios.get(url, { params })
+    // console.log(response.data)
+    return response.data.data
+  } catch (err) {
+    console.log('Failed to get comments from graph api --->', err)
+  }
+}
+
+//TODO: บันทึกไฟล์ .csv ลูกค้าที่สั่งซื้อสินค้าตามรหัส code
+/* 
+  ตรวจสอบ comment ที่ขึ้นต้นด้วย "รหัสสินค้า" ที่กำหนดไว้ หากตรงกันจริง
+  จะทำการเขียนข้อมูลลงในไฟล์ CSV ในรูปแบบของตาราง rows, columns
+  และใช้ fs.appendFileSync() โดยเขียนข้อมูลเข้าไปแทรกในข้อมูลที่มีอยู่แล้วในไฟล์
+*/
+let code = 'ok'
+const fileCSV = 'customer.csv'
+
+function writeFileCSV(comment) {
+  try {
+    console.log(comment)
+    if (
+      String(comment.message).substring(0, code.length).toLowerCase() ==
+      code.toLowerCase()
+    ) {
+      let row =
+        comment.from.id +
+        ',' +
+        comment.from.name +
+        ',' +
+        comment.message +
+        ',' +
+        comment.created_time +
+        '\n'
+      fs.appendFileSync(fileCSV, row, 'utf8')
+    }
+  } catch (e) {
+    console.log('Write file error --->', e)
+  }
 }
 
 //* Main
-async function main() {
-  let result = await getComment() //TODO:
-  console.log(result)
+async function getComment() {
+  let firstRound = true
+  let commentList = []
+
+  setInterval(async () => {
+    try {
+      let newComment = await getCommentAPI()
+      if (firstRound) {
+        // console.log('Initial comment list')
+        // console.log(newComment)
+        fs.writeFileSync(fileCSV, '\ufeffComment_ID,Name,Code,Date\n', 'utf8')
+        commentList = newComment
+        commentList.forEach((comment) => {
+          writeFileCSV(comment)
+        })
+        firstRound = false
+      } else {
+        commentList = await latestComment(commentList, newComment)
+      }
+    } catch (err) {
+      console.log('Failed to get comments --->', err)
+    }
+  }, 10000)
 }
 
-//* Use Main and Exception
-
-try {
-  main()
-} catch (error) {
-  console.log({ message: error.message })
-}
+getComment()
